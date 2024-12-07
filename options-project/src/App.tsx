a620e4b484f459c70077171d305fd34f2d3b9fda
Justin Wang <85368198+jstwng@users.noreply.github.com>
2024-12-07 02:06:36 -0800

    more changes

--- options-project/src/App.tsx
+++ options-project/src/App.tsx
@@ -3,36 +3,58 @@ import Latex from "react-latex";
 import { useState } from "react";
 
 function App() {
-  const blackScholes = "$$C(S_t, t)=N(d_{+})S_t-N(d\\_)Ke^{-r(T-t)}$$";
-  const dPlus =
+  var blackScholes = "$$C(S_t, t)=N(d_{+})S_t-N(d\\_)Ke^{-r(T-t)}$$";
+  var dPlus =
     "$$d_{+}=\\frac{1}{\\sigma\\sqrt{T-t}}[\\ln(\\frac{S_t}{K})+(r+\\frac{\\sigma^2}{2})(T-t)]$$";
-  const dMinus = "$$d\\_=d_{+}-\\sigma\\sqrt{T-t}$$";
+  var dMinus = "$$d\\_=d_{+}-\\sigma\\sqrt{T-t}$$";
 
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState("20");
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setValue(e.target.value);
   }
 
+  const calcValue = parseFloat(value) * 2;
+
   return (
     <>
-      <h1>This is the Black-Scholes Formula</h1>
-      <Latex>{blackScholes}</Latex>
+      <h1>Black-Scholes Formula</h1>
+      <div style={{ display: "inline" }}>
+        <Latex>{blackScholes}</Latex>
+      </div>
+      <h3 style={{ display: "inline" }}>= {calcValue}</h3>
       <br></br>
-      <h3>Where: </h3>
-      <Latex>{dPlus}</Latex>
-      <br></br>
-      <Latex>{dMinus}</Latex>
-      <br></br>
-      <input
-        className="slider"
-        type="range"
-        value={value}
-        onInput={handleChange}
-        min="0"
-        max="40"
-      />
-      <br></br>
-      <h3>{value}</h3>
+      <h3 style={{ display: "inline" }}>Where: </h3>
+      <div style={{ display: "inline" }}>
+        <Latex>{dPlus}</Latex>
+      </div>
+      <h3 style={{ display: "inline", marginLeft: "2px", marginRight: "2px" }}>
+        and
+      </h3>
+      <div style={{ display: "inline" }}>
+        <Latex>{dMinus}</Latex>
+      </div>
+      <div style={{ display: "flex" }}>
+        <h3 style={{ display: "inline", marginRight: "20px" }}>
+          <em>S = </em>{" "}
+          <input
+            className="textbox"
+            type="text"
+            id="username"
+            name="username"
+            value={value}
+            onInput={handleChange}
+          />
+        </h3>
+        <input
+          className="slider"
+          style={{ display: "block", marginTop: "16px" }}
+          type="range"
+          value={value}
+          onInput={handleChange}
+          min="0"
+          max="40"
+        />
+      </div>
     </>
   );
 }
